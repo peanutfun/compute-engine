9135ea87546e5f314fa10735c86460ac5f6e0df8
Lukas Riedel <mail@lukasriedel.com>
2025-11-20 16:23:05 +0100

    Mock climada when building docs

--- doc/conf.py
+++ doc/conf.py
@@ -21,6 +21,9 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# Autodoc
+autodoc_mock_imports = ["climada"]
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
